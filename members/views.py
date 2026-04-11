df92f22c9eea7f5c8f381d66f0160ca5224d3531
Sonu Kumar <85919752+sonu621@users.noreply.github.com>
2026-04-11 13:18:16 +0000

    My Tennis Club Initinal Practice Project

---
+++ members/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
+
+def members(request):
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+  mymembers = Members.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymembers': mymembers
+  }
+
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))
