dfb389bc10b1067b06000bbdcb5e22bdf5156bcd
Sonu Kumar <85919752+sonu621@users.noreply.github.com>
2026-04-13 13:15:25 +0000

    Clean up repo and update models/admin

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Members
 
 # Register your models here.
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'firstname', 'lastname', 'phone', 'joined_date')
+
+admin.site.register(Members, MembersAdmin)
+
+
